c839769d55245af84bd2678051140722b1c2afe4
GAMING PC - k1r <secdev.kir@gmail.com>
2024-02-20 20:16:59 +0800

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/templates/**/*.html",
+    "./app/static/js/**/*.js",
+    "./app/static/css/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
