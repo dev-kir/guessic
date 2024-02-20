8daa53fdb2e04a441b5232ca7c340239c2420db7
GAMING PC - k1r <secdev.kir@gmail.com>
2024-02-20 19:48:18 +0800

    setup the base of flask python framework p2

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from app import routes
 app = Flask(__name__)
 
 # Register your routes
-app.register_blueprint(routes.home)
-app.register_blueprint(routes.about)
-app.register_blueprint(routes.contact)
+app.register_blueprint(routes.main)
+# app.register_blueprint(routes.home)
+# app.register_blueprint(routes.about)
+# app.register_blueprint(routes.contact)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,22 @@
 from flask import Blueprint, render_template
 
-home = Blueprint('home', __name__, template_folder='templates')
-about = Blueprint('about', __name__, template_folder='templates')
-contact = Blueprint('contact', __name__, template_folder='templates')
+main = Blueprint('main', __name__, template_folder='templates')
+# home = Blueprint('home', __name__, template_folder='templates')
+# about = Blueprint('about', __name__, template_folder='templates')
+# contact = Blueprint('contact', __name__, template_folder='templates')
 
-@home.route('/')
-def home_route():
-    return render_template('home.html')
+# @home.route('/')
+# def home_route():
+#     return render_template('home.html')
 
-@about.route('/about')
-def about_route():
-    return render_template('about.html')
+# @about.route('/about')
+# def about_route():
+#     return render_template('about.html')
 
-@contact.route('/contact')
-def contact_route():
-    return render_template('contact.html')
+# @contact.route('/contact')
+# def contact_route():
+#     return render_template('contact.html')
+
+@main.route('/')
+def index():
+    return render_template('index.html')
