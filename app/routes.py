fb0870f31075994d01d4d2bfb90006a234279cbc
dev-kir <secdev.kir@gmail.com>
2024-02-27 07:54:57 +0800

    add limiter to avoid bot request

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from flask import Flask, Blueprint, render_template, redirect, url_for, request
 from app.forms import inputForm
+from flask_limiter.util import get_remote_address
+from flask_limiter import Limiter
+import app
 
 main = Blueprint('main', __name__, template_folder='templates')
 # home = Blueprint('home', __name__, template_folder='templates')
@@ -8,12 +11,18 @@ main = Blueprint('main', __name__, template_folder='templates')
 # def home_route():
 #     return render_template('home.html')
 
+limiter = Limiter(
+    key_func = get_remote_address,
+    storage_uri = "memory://"
+)
 
-@main.route('/', methods = ['GET', 'POST'])
+
+@main.route('/', methods = ['GET'])
 def index():
     return render_template('index.html', form = inputForm())
 
 @main.route('/result', methods = ['GET', 'POST'])
+@limiter.limit("2 per minute")
 def result():
     form = inputForm()
     if form.validate_on_submit():
@@ -23,7 +32,6 @@ def result():
         for i in range(10000):
             line = f"{number:08d}{i:04d}"
             if (gender == 'male' and i % 2 != 0) or (gender == 'female' and i % 2 == 0):
-                # line = str(int(line) + 1)
                 processed_lines.append(line)
         result_data = '\n'.join(processed_lines)
         return render_template('result.html', result_data=result_data)
